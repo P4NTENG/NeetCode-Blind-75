abebaea08651bc10b4293f62e5768c8ffd9e06d9
panteng <ptis307@naver.com>
2026-05-27 01:04:03 +0900

    add hint

--- sliding-window/minimun-window-substring/solution.py
+++ sliding-window/minimun-window-substring/solution.py
@@ -1,20 +1,27 @@
 def minWindow(s: str, t: str) -> str:
     left = 0
-    t_freq = {}
+    goal_freq = {}
+    freq = {}
     answer = s
     for char in t:
-        t_freq[char] = t_freq.get(char, 0) + 1
+        goal_freq[char] = goal_freq.get(char, 0) + 1
 
     for right in range(0, len(s)):
-        check_freq = t_freq.copy()
-        for char in s[left : right + 1]:
-            check_freq[char] = check_freq.get(char, 0) - 1
-        if all(v <= 0 for v in check_freq.values()):
+        freq[s[right]] = freq.get(s[right], 0) + 1
+
+        diff = dict.fromkeys(goal_freq.keys(), 0)
+
+        for k in diff.keys():
+            diff[k] = goal_freq[k] - freq.get(k, 0)
+
+        if all(v <= 0 for v in diff.values()):
+            freq[s[left]] -= 1
             left += 1
 
-    answer = min(answer, s[left : right + 1])
+        answer = min(answer, s[left : right + 1])
 
     return answer
 
 
 print(minWindow("OUZODYXAZV", "XYZ"))
+# 조건을 만족한 문자만 세기
--- sliding-window/minimun-window-substring/test.py
+++ sliding-window/minimun-window-substring/test.py
@@ -1,3 +1,5 @@
 a = {"a": 1, "b": 2}
-print(a)
+b = {"a": 3, "b": 4}
+
+print(b - a)
 print(dict.fromkeys(a.keys(), 0))
